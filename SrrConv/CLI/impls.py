e06da3dfc7dfe416245f2e66fca59f23dc9760df
Richard Moore <richarda.mooreiii@gmail.com>
2026-03-10 13:34:40 -0400

    v2.0.2
    v2.0.2:
        - Implemented logging system
            - Added logging messages to existing functions
        - Migrated code from __main__ into a dedicated file allowing it to be ran as a module (folder) or script (file).
        - Added `_determine_extension_from_bytes` to fingerprint filetypes with unknown name
        - Simplified hashing function to not require -s argument (`SrrConv hash <string>` instead of `SrrConv hash -s <string>`)
        - Cleaned up CLI help messages
        - Added defaults configuration.
            - Added verb for "defaults"
                - List: will list current defaults
                - Set: will update a default to a new value
            - Writing to log file will use default `log_file` if no file was specified
            - BigFile will use default `big_file` if no file was specified
            - Default output will use `out_path`
            - Hash will use default `hash_manifest` if no file was specified

--- SrrConv/CLI/impls.py
+++ SrrConv/CLI/impls.py
@@ -1,97 +1,260 @@
 from pathlib import Path
 from SrrConv.CrystalDynamics.bigfile import BigFile
 from SrrConv.CrystalDynamics.hash import hash_str
+from SrrConv import __version__
+
+import logging
+import pickle
+
+DEFAULT_PROPERTIES = {
+    "big_file": "./bigfilehd.dat",
+    "hash_manifest": "./hash.manifest",
+    "log_file": "./output.log",
+    "out_path": "./"
+}
+
+def _determine_extension_from_bytes(data):
+    """Determine the filetype extension utilizing the file signature"""
+    match data[:3]:
+        case b'DDS':
+            return ".dds"
+        case b"SRM":
+            return ".srm"
+    return ".bin"
+
+def get_default_property(property, logger):
+    """Return a default property if one exists"""
+    defaults = Path("./config.defaults")
+    if not defaults.exists():
+        if logger: logger.debug("No defaults file exists...Creating defaults")
+        generate_new_defaults()
+
+    if logger: logger.debug("Loading config defaults from path: ./config.defaults")
+    with open("./config.defaults", "rb") as file:
+        properties = pickle.load(file)
+        if property not in properties:
+            if logger: logger.error("No property `{property}` found in defaults")
+        return properties.get(property, None)
+
+def init_log(args):
+    """Initialize the logger using any arguments passed in the command"""
+    logger = logging.getLogger("SrrConv")
+    log_level = logging.INFO
+    write_log = args.write_log
+
+    match args.log_level:
+        case "debug": log_level   = logging.DEBUG
+        case "error": log_level   = logging.ERROR
+        case "warning": log_level = logging.WARN
+        case "none": return logger                  # Silent output
+    logger.setLevel(log_level)
+
+    formatter = logging.Formatter("%(levelname)s: %(message)s")
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    if write_log != None:
+        file_handler = logging.FileHandler(write_log, mode="w")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+    return logger
+
+def generate_new_defaults():
+    """When no config.defaults exists, create them"""
+    with open("config.defaults", "wb") as fileout:
+        pickle.dump(DEFAULT_PROPERTIES, fileout)
+
+def list_defaults_impl(args):
+    """Lists the current default parameters stored inside of config.defaults"""
+    logger = init_log(args)
+    logger.debug(f"SRRConv {__version__} - {__name__}.list_defaults")
+    logger.debug(f"{args}")
+    
+    defaults = Path("./config.defaults")
+    if not defaults.exists():
+        logger.debug("No defaults file exists...Creating defaults")
+        generate_new_defaults()
+
+    logger.debug("Loading config defaults from path: ./config.defaults")
+    with open("./config.defaults", "rb") as file:
+        logger.info("Current defaults:")
+        properties = pickle.load(file)
+        for (name, value) in properties.items():
+            logger.info(f"\t{name}: {value}")
+    
+
+def assign_default_impl(args):
+    """Change a default's value if the default exists"""
+    logger = init_log(args)
+    logger.debug(f"SRRConv {__version__} - {__name__}.assign_default_impl")
+    logger.debug(f"{args}")
+
+    default = args.default
+    value = args.value.replace("\\", "/")
+    
+    defaults = Path("./config.defaults")
+    if not defaults.exists():
+        logger.debug("No defaults file exists...Creating defaults")
+        generate_new_defaults()
+
+    properties = {}
+
+    logger.debug("Loading config defaults from path: ./config.defaults")
+    with open("./config.defaults", "rb") as filein:
+        properties = pickle.load(filein)
+        if default in properties:
+            logger.info(f"Reassigning `{default}` to `{value}`")
+            properties[default] = value
+        else:
+            logger.error("Unable to find property: {default}")
+
+    logger.debug("Saving updated config defaults to path: ./config.defaults")
+    with open("./config.defaults", "wb") as fileout:
+        pickle.dump(properties, fileout)
 
 def hash_impl(args):
     """Given a string return it's hash"""
-    print(hex(hash_str(args.string)), f" {args.string}")
+    logger = init_log(args)
+    logger.debug(f"SRRConv {__version__} - {__name__}.hash_impl")
+    logger.debug(f"{args}")
+    logger.info(f"{hex(hash_str(args.string))}\t{args.string}")
+
+
+def _get_bigfile_data(big_path: str, index_type: str, input: str, logger: logging.Logger) -> None | bytearray:
+    """Retrieve data from a big file by hash or filename"""
+    logger.info("Loading BigFile: {bigfile}")
+    bigfile = BigFile.from_file(big_path)
+
+    if index_type == "hash":
+        logger.info("Attempting to get the data for entry with hash: {input}...")
+        return bigfile.get_data_from_hash(int(input, 0x10))
+    else:
+        logger.info("Attempting to get the data for entry with name: {input}...")
+        return bigfile.get_data_from_string(input)
+
+def _prep_output_directory(input: str, outpath: Path, no_paths: bool, logger: logging.Logger):
+    """Create the final file path and ensure it's parent directories exist"""
+    final_path = ""
+
+    if no_paths:
+        logger.debug("No path flag is set")
+        final_path = outpath / Path(input).name
+        outpath.mkdir(parents=True, exist_ok=True)
+    else:
+        logger.debug("No path flag is not set")
+        final_path = outpath / input
+        final_path.parent.mkdir(parents=True, exist_ok=True)
+
+    logger.debug(f"Final path is: {final_path}")
+    return final_path
 
 def extract_file_impl(args):
     """
         Given a target BigFile and a filename or hash extract
         the data and store it in the specified outpath location
     """
+    logger = init_log(args)
+    logger.debug(f"SRRConv {__version__} - {__name__}.extract_file_impl")
+    logger.debug(f"{args}")
+
     input = args.input
-    bigfile = args.bigfile
-    outpath = Path(args.outpath)
+    bigfile  = args.bigfile if args.bigfile else get_default_property("big_file", logger)
+    outpath  = Path(args.outpath) if args.outpath else Path(get_default_property("out_path", logger))
     no_paths = args.no_paths
-    index_type = args.index_type
+    type = args.type
 
-    bigfile = BigFile.from_file(bigfile)
-    if index_type == "hash":
-        data = bigfile.get_data_from_hash(int(input, 0x10))
-    else:
-        data = bigfile.get_data_from_string(input)
-    final_path = ""
 
-    if no_paths:
-        final_path = outpath / Path(input).name
-        outpath.mkdir(parents=True, exist_ok=True)
-    else:
-        final_path = outpath / input
-        final_path.parent.mkdir(parents=True, exist_ok=True)
+    data = _get_bigfile_data(bigfile, type, input, logger)
+    if data == None:
+        logger.error("Failed to load data from big file! Aborting...")
+        return
+    logger.info("Successfully loaded data from the BigFile")
 
+    final_path = _prep_output_directory(input, outpath, no_paths, logger)
+
+    logger.info(f"Writing {hex(len(data))} bytes to: {final_path}")
     with open(final_path, "wb") as file_out:
-        if data:
-            file_out.write(data)
-        else:
-            # TODO: Error getting data
-            return None
+        file_out.write(data)
 
-def extract_all_w_manifest_cross_ref(args):
-    """
-        If the --known flag is not set any binary blob that is not
-        present in the hash manifest file will be extracted using
-        it's hash as it's name
-    """
-    input = args.input
-    bigfile = args.bigfile
-    outpath = Path(args.outpath)
-    no_paths = args.no_paths
-    bigfile = BigFile.from_file(bigfile)
-    
+    logger.info(f"Success!")
+
+def _get_manifest_filenames(input: str, logger: logging.Logger):
+    """Load the list of file name from the manifest file"""
+    logger.info(f"Loading filenames from the manifest file: {input}")
     hash_map = {}
 
-    # Create the literal hash map [hash : filename]
     manifest_lines = open(input, "r").readlines()
     for line in manifest_lines:
         name = line.replace('\n', '').replace('\r', '')
         hash_map[hash_str(name)] = name
 
+    logger.debug(f"Loaded: {len(hash_map)} filenames")
+    return hash_map
+
+def extract_all_w_manifest_cross_ref(args):
+    """
+        Extracts all the files found in a BigFile.
+        If a hash is encountered that doesn't match a file name in the
+        manifest file. The hashes value will be converted to a string
+        instead.
+
+        If the --known flag is not set any binary blob that is not
+        present in the hash manifest file will be extracted using
+        it's hash as it's name
+    """
+    logger = init_log(args)
+    logger.debug(f"SRRConv {__version__} - {__name__}.extract_all_w_manifest_cross_ref")
+    logger.debug(f"{args}")
+
+    input    = args.input if args.input else get_default_property("hash_manifest", logger)
+    bigfile  = args.bigfile if args.bigfile else get_default_property("big_file", logger)
+    outpath  = Path(args.outpath) if args.outpath else Path(get_default_property("out_path", logger))
+    no_paths = args.no_paths
+
+    logger.info(f"Loading BigFile: {bigfile}")
+    bigfile = BigFile.from_file(bigfile)
+    
+    hash_map = _get_manifest_filenames(input, logger)
 
     for hash in bigfile.entries.keys():
+        data = bigfile.get_data_from_hash(hash)
+        if data is None: # Not really possible, but it's proper to check for None
+            logger.debug(f"Hash not found, skipping...")
+            continue
+
         if hash in hash_map.keys():
             name = hash_map[hash]
         else:
-            name = f"{hex(hash)}.bin"
+            logger.debug(f"Hash miss [{hex(hash)[2:]}]")
+            extension = _determine_extension_from_bytes(data)
+            logger.debug(f"Hash determined filetypes as: {extension[1:]}")
+            name = f"{hex(hash)[2:]}{extension}"
 
-        data = bigfile.get_data_from_hash(hash)
-        if data is None:    # Hash may be in a different big file
-            continue
+        final_path = _prep_output_directory(name, outpath, no_paths, logger)
 
-        if no_paths:
-            final_path = outpath / Path(name).name
-            outpath.mkdir(parents=True, exist_ok=True)
-        else:
-            final_path = outpath / name
-            final_path.parent.mkdir(parents=True, exist_ok=True)
-
+        logger.info(f"{final_path}")
         with open(final_path, "wb") as file_out:
             file_out.write(data)
 
-
+# TODO: Update
 def extract_w_manifest_cross_ref(args):
     """
         If requested to only extract known files, (If --known flag set) 
         extract any file present in the BigFile that can be cross 
         referenced in the hash manifest
     """
-    input = args.input
-    bigfile = args.bigfile
-    outpath = Path(args.outpath)
-    no_paths = args.no_paths
-    bigfile = BigFile.from_file(bigfile)
+    logger = init_log(args)
+    logger.debug(f"SRRConv {__version__} - {__name__}.extract_w_manifest_cross_ref")
+    logger.debug(f"{args}")
 
+    input    = args.input
+    bigfile  = args.bigfile if args.bigfile else get_default_property("big_file", logger)
+    outpath  = Path(args.outpath) if args.outpath else get_default_property("out_path", logger)
+    no_paths = args.no_paths
+
+    bigfile  = BigFile.from_file(bigfile)
     manifest_lines = open(input, "r").readlines()
     for line in manifest_lines:
         name = line.replace('\n', '').replace('\r', '')
--- SrrConv/srrconverter.py
+++ SrrConv/srrconverter.py
@@ -3,45 +3,72 @@ from . import __version__
 
 from SrrConv.CLI.impls import *
 
+from pathlib import Path
+
 def parse_args():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(help='subcommand help', required=True)
+
+    if not Path("./config.defaults").exists():
+        generate_new_defaults()
+
+    # Global Flags
+    parser.add_argument(
+        "-l", 
+        "--log-level", 
+        help="set the logging level. (lower is more verbose) default: Info", 
+        choices=("none", "error", "warning", "info", "debug"), 
+        default="info"
+    )
+    log_default = get_default_property("log_file", None)
+    parser.add_argument("-w", "--write-log", metavar="<FILE>", action="store", nargs="?", help="write any log output to a file", const=log_default)
     
     # Version Command
-    version = subparsers.add_parser('version', help='Displays current version information')
+    version = subparsers.add_parser('version', help='displays program version information')
     version.set_defaults(func=lambda _args: print("Version: ", __version__))
+    
+    # Defaults Command
+    defaults = subparsers.add_parser('defaults', help='view and modify system defaults')
+    defaults_subcommands = defaults.add_subparsers()
+    list = defaults_subcommands.add_parser('list', help='list the current defaults')
+    list.set_defaults(func=list_defaults_impl)
+
+    set = defaults_subcommands.add_parser('set', help='set a default value')
+    set.add_argument("default", metavar="<default>", help="determines the default property wanting to be set.")
+    set.add_argument("value", metavar="<value>", help="determines the new value to be assigned.")
+    set.set_defaults(func=assign_default_impl)
 
     # Hash Command
-    hash = subparsers.add_parser('hash', help='Calculate the hash for a string')
-    hash.add_argument("-s", "--string", help="The string to be hashed.")
+    hash = subparsers.add_parser('hash', help='calculate the SR3 hash of a given string')
+    hash.add_argument("string", metavar='<string>', help="the string to be hashed.")
     hash.set_defaults(func=hash_impl)
 
     # Extract command
-    extract = subparsers.add_parser('extract', help='Rips a file from a bigfile using a string name')
-    extract_subparser = extract.add_subparsers(help='Extract Target', required=True)
+    extract = subparsers.add_parser('extract', help='extract a single file using a hash or string path')
+    extract_subparser = extract.add_subparsers(help='extract Target', required=True)
     
     # Extract Manifest  
-    extract_manifest = extract_subparser.add_parser("manifest", help="Extract all files found in a hash.manifest file")
-    extract_manifest.add_argument("-i", "--input", help="Specifies the manifest file to use", required=True)
-    extract_manifest.add_argument("-b", "--bigfile", help="Specifies the big file to extract from", required=True)
-    extract_manifest.add_argument("-k", "--known-only", action="store_true", help="Extract only filenames found in the manifest")
-    extract_manifest.add_argument("-n", "--no-paths", action="store_true", help="Ignore internal subpaths")
-    extract_manifest.add_argument("-o", "--outpath", help="The destination folder to write the file(s). Default = current directory", default="./")
+    extract_manifest = extract_subparser.add_parser("manifest", help="extracts all files listed within a `hash.manifest` file.")
+    extract_manifest.add_argument("-i", "--input", help="path to the manifest file containing filenames.")
+    extract_manifest.add_argument("-b", "--bigfile", help="path to the bigfile from which to extract. If unspecified `config.defaults` is used.")
+    extract_manifest.add_argument("-k", "--known-only", action="store_true", help="extract only files listed within the manifest file; ignore others.")
+    extract_manifest.add_argument("-n", "--no-paths", action="store_true", help="flattens the output directory, preventing subdirectory creation.")
+    extract_manifest.add_argument("-o", "--outpath", help="specify the output directory where the extracted file will be saved. default: ./")
     extract_manifest.set_defaults(func=extract_manifest_impl)
 
     # Extract File
-    extract_file = extract_subparser.add_parser("file", help="Extract a single file using a hash or string path")
+    extract_file = extract_subparser.add_parser("file", help="extract a single file using a hash or string path")
     extract_file.add_argument(
         "-t", 
         "--type", 
         choices=("hash", "string"), 
-        help="Determines if the search parameter is a hash or a string. Default string",
+        help="specify whether `input` is a hash or a string. default: string",
         default="string"
     )
-    extract_file.add_argument("-i", "--input",  help="The hash/filename that you wish to extract", required=True)
-    extract_file.add_argument("-b", "--bigfile", help="Big file that contains the file", required=True)
-    extract_file.add_argument("-n", "--no-paths", action="store_true", help="Ignore internal subpaths")
-    extract_file.add_argument("-o", "--outpath", help="The folder to write the file. Default = current directory", default="./")
+    extract_file.add_argument("input", help="Hash/String of the data  to extract from the bigfile")
+    extract_file.add_argument("-b", "--bigfile", help="path to the bigfile from which to extract. If unspecified `config.defaults` is used.")
+    extract_file.add_argument("-n", "--no-paths", action="store_true", help="flattens the output directory, preventing subdirectory creation.")
+    extract_file.add_argument("-o", "--outpath", help="specify the output directory where the extracted file will be saved. default: ./", default="./")
     extract_file.set_defaults(func=extract_file_impl)
 
     args = parser.parse_args()
